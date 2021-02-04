ee71bd117a758467b0ff52b3ef9f710690fcb653
saladinjake <juwavictor@gmail.com>
2021-02-04 22:48:11 +0100

    code refactor: Implementing interfaace for front controller and upload controller

--- project/app/Services/Contracts/UploadServiceInterface.php
+++ project/app/Services/Contracts/UploadServiceInterface.php
@@ -2,8 +2,11 @@
 
 namespace App\Services\Contracts;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
 Interface UploadServiceInterface
 {
-    public function uploadViaDevice();
-    public function uploadViaApi();
+    public function uploadViaDevice(Request $request);
+    public function uploadViaApi(Request $request);
 }
