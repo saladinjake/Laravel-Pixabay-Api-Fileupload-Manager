4b285bf1f7038133d9583c6010ac255b211b3fa2
saladinjake <juwavictor@gmail.com>
2021-02-04 21:21:37 +0100

    cr-#1(codeRefactor: Add thin controller refactoring to backend http navigator:uploadcontroller)

--- project/app/Http/Controllers/UploaderController.php
+++ project/app/Http/Controllers/UploaderController.php
@@ -1,19 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-// use Illuminate\Http\Request;
-// use Validator;
+use Illuminate\Http\Request;
+use Validator;
 use App\Services\UploadService;
 
 class UploaderController extends Controller
 {
+    public function __construct(Request $request){
+      $this->requestHandler = $request;
+    }
     // method dependency injection
     public function uploadViaDevice(UploadService $uploader)
     {
-
+       return $uploader->uploadViaDevice($this->requestHandler);
     }
 
-    public function uploadViaApi(Request $request){
-       
+    public function uploadViaApi(UploadService $uploader){
+        return $uploader->uploadViaApi($this->requestHandler);
     }
 }
